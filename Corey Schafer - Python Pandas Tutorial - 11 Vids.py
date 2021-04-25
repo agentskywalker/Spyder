22d81598427109d2638ca78abe249c083e58b9f7
agentskywalker <agentskywalker@gmail.com>
2021-04-25 21:23:41 +0200

    25.04 Last update

--- Corey Schafer - Python Pandas Tutorial - 11 Vids.py
+++ Corey Schafer - Python Pandas Tutorial - 11 Vids.py
@@ -185,23 +185,78 @@ df.loc[0:4, "Hobbyist"]  # Get a range of response for Hobbyist for 1-5 rows
 df.loc[0:4, "Hobbyist":"Employment"]
 
 
-#%%
+#%% ################ Video 3 ##################################################
+# Python Pandas Tutorial (Part 3): Indexes - How to Set, Reset, and Use Indexes
+###############################################################################
+# Create and use custom indexed
+
+people = {
+    "first": ["Corey", "Jane", "John"],
+    "last": ["Schafer", "Doe", "Doe"],
+    "email": ["CoreyMSchafer@gmail.com", "JaneDoe@email.com", "JohnDoe@email.com"],
+}
+
+import pandas as pd
+
+df = pd.DataFrame(people)
+df
+
+df["email"]
+
+df.set_index("email")  # Does NOT update the actual panda
+df.set_index("email", inplace=True)  # Updates the actual panda
+
+df
+
+# df.loc[0]      # Gives error after updating the index
+df.iloc[0]  # Does not give error
+df.loc["CoreyMSchafer@gmail.com", "last"]  # Works using the email as index
+
+# Reset Index
+df.reset_index(inplace=True)
+df.loc[0]
+
+#%% Index operations using Stack Overflow data file
+
+import pandas as pd
+
+# Default index is gone and the column Respondent is used as index
+df = pd.read_csv("data2019/survey_results_public.csv", index_col="Respondent")
+
+# Default index is gone and the column Column is used as index
+schema_df = pd.read_csv("data2019/survey_results_schema.csv", index_col="Column")
+pd.set_option("display.max_columns", 85)
+pd.set_option("display.max_rows", 85)
+
+df.head()
+schema_df
+
+
+#%% Only 1 command comes in display in the interactive window
+############# Check for corresponding settings #######
+
+# schema_df.loc["MainBranch"]
+# schema_df.loc["SOVisit1st"]
+schema_df.loc["ITperson"]
+
 
 #%%
+# schema_df.loc["MgrIdiot", 'QuestionText']
+schema_df.loc["ITperson", "QuestionText"]
+
+#%% Sort the index alphabetically - TEMPORARILY
+
+schema_df.sort_index()
+schema_df.sort_index(ascending=False)
 
 
-#%%
+#%% Sort the index alphabetically - PERMANENTLY
+schema_df.sort_index(inplace=True)
+schema_df
 
-
-#%%
-
-#%%
-
-
-#%%
-
-
-#%%
+#%% ################ Video 4 ##################################################
+# Python Pandas Tutorial (Part 4): Filtering - Using Conditionals to Filter Rows and Columns
+###############################################################################
 
 #%%
 
